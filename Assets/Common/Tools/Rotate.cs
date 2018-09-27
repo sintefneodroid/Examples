ca3ab7bebeca3d708a817da6501436a3779a9de2
Christian Heider Nielsen <mrnaah@gmail.com>
2018-09-27 15:16:50 +0200

    Updated Neodroid

---
+++ Assets/Common/Tools/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Excluded.Hide.SharpShadowLight.Scripts.Utilities {
+  /// <inheritdoc />
+  /// <summary>
+  /// </summary>
+  public class Rotate : MonoBehaviour {
+    [SerializeField] Vector3 _rotation_vector = Vector3.up;
+    [SerializeField] float _speed = 0.1f;
+
+    void FixedUpdate() { this.transform.Rotate(this._rotation_vector, this._speed); }
+  }
+}
