ccc6ce6429cf40df29ec1166b9b4b4472b3c8318
Christian Heider Nielsen <mrnaah@gmail.com>
2019-04-23 16:45:30 +0200

    Droid update

---
+++ Assets/SceneAssets/Experiments/Walker2d/SpriteShadow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace SceneAssets.Experiments.Walker2d {
+  /// <inheritdoc />
+  /// <summary>
+  /// </summary>
+  [RequireComponent(typeof(SpriteRenderer)),ExecuteAlways]
+  public class SpriteShadow : MonoBehaviour {
+    [SerializeField] bool receive_shadows = true;
+    [SerializeField] ShadowCastingMode cast_shadows = ShadowCastingMode.On;
+
+    SpriteRenderer _renderer;
+
+    void Awake() {
+      this._renderer = this.GetComponent<SpriteRenderer>();
+
+      this._renderer.shadowCastingMode = this.cast_shadows;
+
+      this._renderer.receiveShadows = this.receive_shadows;
+    }
+  }
+}
